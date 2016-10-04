27a6bd296ffbdb095c598d5d20a63a5d12996515
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-10-04 16:20:11 -0700

    Set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :markets, except: [:destroy]
+
+  resources :vendors do
+    resources :products
+    resources :sales, except: [:destroy, :update, :edit]
+  end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
