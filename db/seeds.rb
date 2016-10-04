76a99b01167a1c5106adfff7c808c48f966cba28
Kelly Tran <trankphuong@gmail.com>
2016-10-04 14:46:39 -0700

    Pulled data from CSV files to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+CSV.read('seed_csvs/markets.csv').each do |line|
+  market_hash = { ref_id: line[0].to_i, name: line[1], address: line[2], city: line[3], county: line[4], state: line[5], zip: line[6]}
+  Market.create(market_hash)
+end
+
+CSV.read('seed_csvs/vendors.csv').each do |line|
+  vendor_hash = { ref_id: line[0].to_i, name: line[1], num_employees: line[2].to_i, market_id: line[3].to_i}
+  Vendor.create(vendor_hash)
+end
+
+CSV.read('seed_csvs/products.csv').each do |line|
+  product_hash = { ref_id: line[0].to_i, name: line[1], vendor_id: line[2].to_i}
+  Product.create(product_hash)
+end
+
+CSV.read('seed_csvs/sales.csv').each do |line|
+  sale_hash = { ref_id: line[0].to_i, amount: line[1].to_i, purchase_time: DateTime.parse(line[2]), vendor_id: line[3].to_i, product_id: line[4].to_i}
+  Sale.create(sale_hash)
+end
