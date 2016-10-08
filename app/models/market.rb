167660e3b3f7be2f60b8d50dbf148286122da01c
Kelly Tran <trankphuong@gmail.com>
2016-10-08 00:02:57 -0700

    Modified some semantic tags and structures in order to sync with CSS style sheet easily.

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,8 +1,6 @@
 class Market < ActiveRecord::Base
   has_many :vendors
 
-  validates :ref_id, uniqueness: true
-
   def all_vendors
     self.vendors
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,4 @@ class Product < ActiveRecord::Base
   belongs_to :vendor
   has_many :sales
 
-  # validates_presence_of :name
 end
